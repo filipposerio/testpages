15e319e9bcb027922e8ccf286ec2297d5956dff8
filipposerio <filippo.serio@gmail.com>
2025-01-07 15:15:00 +0100

    Add files via upload

---
+++ files/bilanci/frasi.js
@@ -0,0 +1,49 @@
+lFrasi=
+[
+    {
+        anno : "2023",
+        testo : "Sostenibilità finanziaria e ambientale per perseguire la creazione di valore con una struttura finanziaria solida ed equilibrata, affrontando e sostenendo con impegno le sfide del cambiamento climatico. "
+    },
+    {
+        anno : "2022",
+        testo : "Viviamo in un mondo sempre più interconnesso dove le aziende che continueranno a prosperare nel lungo periodo saranno quelle in grado di agire collettivamente, creando e condividendo valore con tutti gli stakeholder"
+    },
+    {
+        anno : "2021",
+        testo : "Il mondo ha di fronte a sé una sfida importante, che richiede un impegno immediato e concreto. L’elettricità è la forma di energia più efficiente, sicura ed economica, ed è la chiave, con le fonti rinnovabili e lo storage, per la completa decarbonizzazione del nostro pianeta"
+    },
+    {
+        anno : "2020",
+        testo : "Un anno impegnativo come il 2020, caratterizzato dall’emergenza pandemica, ci ha fatto fare un viaggio nel futuro agendo da acceleratore di importanti trend legati alla transizione energetica: decarbonizzazione, elettrificazione e digitalizzazione."
+    },
+    {
+        anno : "2019",
+        testo : "Cambiamento climatico, crescita della popolazione mondiale, accelerazione tecnologica, mutamenti negli equilibri geopolitici sono tra le principali forze che caratterizzano il cambiamento d’epoca che stiamo vivendo."
+    },
+    {
+        anno : "2018",
+        testo : "“Shaper e leader”: artefici e guida della transizione energetica."
+    },
+    {
+        anno : "2017",
+        testo : "In scenari sempre più mutevoli e competitivi, anche grazie all’accelerazione impressa dall’evoluzione tecnologica, le imprese sono chiamate a cambiare molto velocemente. Nella definizione della propria visione strategica, così come nella sua attuazione."
+    },
+    {
+        anno : "2016",
+        testo : " La sostenibilità è parte integrante del nostro modo di essere impresa, e ci guida nel trovare soluzioni sempre nuove per ridurre l’impatto ambientale delle nostre attività, per crescere assieme alle comunità di cui siamo parte, per aumentare la sicurezza dei nostri colleghi e dei nostri fornitori."
+    },
+    {
+        anno : "2015",
+        testo : "Il 25 settembre 2015, l’Organizzazione delle Nazioni Unite (ONU) ha definitivamente adottato i nuovi Obiettivi di Sviluppo Sostenibile (SDGs) al 2030. Un invito alle aziende a dare il proprio contributo – anche attraverso la capacità d’innovazione – per affrontare le sfide dello sviluppo sostenibile, quali la povertà, il diritto all’istruzione, l’accesso al lavoro, la parità di genere, la disponibilità universale delle risorse idriche, l’accesso all’energia e i cambiamenti climatici."
+    },
+    {
+        anno : "2014",
+        testo : "Essere sostenibili oggi vuol dire essere competitivi domani; significa creare valore duraturo nel tempo rispondendo attraverso l’offerta di prodotti e servizi e attraverso le relazioni con clienti e territorio – alle esigenze delle comunità in cui l’Azienda opera."
+    },
+    {
+        anno : "2013",
+        testo : "Le imprese, soprattutto le grandi multinazionali, sono sempre più uno snodo cruciale di incontro e confronto tra mondo economico e società. Per questo motivo la Corporate Social Responsibility (CSR) è una priorità ineludibile per i leader aziendali nella realizzazione di un modello imprenditoriale efficace e virtuoso. "
+    },
+
+
+]
